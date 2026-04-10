1565e9cf428f55e2ab25128a3d944f771c2469e2
ODC_TPILON <tpilon@odc.fr>
2026-04-10 17:19:45 +0200

    fix: supprimer les opérations sur tables inexistantes dans migration 20260324081857

--- packages/db/prisma/migrations/20260324081857_add_ged_classification/migration.sql
+++ packages/db/prisma/migrations/20260324081857_add_ged_classification/migration.sql
@@ -1,13 +1,4 @@
 -- DropForeignKey
-ALTER TABLE "channels" DROP CONSTRAINT "channels_cluster_id_fkey";
-
--- DropForeignKey
-ALTER TABLE "cluster_members" DROP CONSTRAINT "cluster_members_cabinet_id_fkey";
-
--- DropForeignKey
-ALTER TABLE "cluster_members" DROP CONSTRAINT "cluster_members_cluster_id_fkey";
-
--- DropForeignKey
 ALTER TABLE "document_tags" DROP CONSTRAINT "document_tags_document_id_fkey";
 
 -- DropForeignKey
@@ -29,47 +20,13 @@ ALTER TABLE "folders" DROP CONSTRAINT "folders_cabinet_id_fkey";
 ALTER TABLE "folders" DROP CONSTRAINT "folders_parent_id_fkey";
 
 -- DropForeignKey
-ALTER TABLE "message_reactions" DROP CONSTRAINT "message_reactions_cabinet_id_fkey";
-
--- DropForeignKey
-ALTER TABLE "message_reactions" DROP CONSTRAINT "message_reactions_message_id_fkey";
-
--- DropForeignKey
-ALTER TABLE "message_reactions" DROP CONSTRAINT "message_reactions_user_id_fkey";
-
--- DropForeignKey
-ALTER TABLE "message_reports" DROP CONSTRAINT "message_reports_message_id_fkey";
-
--- DropForeignKey
-ALTER TABLE "messages" DROP CONSTRAINT "messages_channel_id_fkey";
-
--- DropForeignKey
 ALTER TABLE "tags" DROP CONSTRAINT "tags_cabinet_id_fkey";
-
--- AlterTable
-ALTER TABLE "channels" ALTER COLUMN "id" DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE "cluster_members" ALTER COLUMN "id" DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE "clusters" ALTER COLUMN "id" DROP DEFAULT;
 
 -- AlterTable
 ALTER TABLE "export_jobs" ALTER COLUMN "id" DROP DEFAULT;
 
 -- AlterTable
 ALTER TABLE "folders" ALTER COLUMN "id" DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE "message_reactions" ALTER COLUMN "id" DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE "message_reports" ALTER COLUMN "id" DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE "messages" ALTER COLUMN "id" DROP DEFAULT,
-ALTER COLUMN "updated_at" DROP DEFAULT;
 
 -- AlterTable
 ALTER TABLE "tags" ALTER COLUMN "id" DROP DEFAULT;
@@ -97,27 +54,3 @@ ALTER TABLE "export_jobs" ADD CONSTRAINT "export_jobs_cabinet_id_fkey" FOREIGN K
 
 -- AddForeignKey
 ALTER TABLE "export_jobs" ADD CONSTRAINT "export_jobs_requested_by_fkey" FOREIGN KEY ("requested_by") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "cluster_members" ADD CONSTRAINT "cluster_members_cluster_id_fkey" FOREIGN KEY ("cluster_id") REFERENCES "clusters"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "cluster_members" ADD CONSTRAINT "cluster_members_cabinet_id_fkey" FOREIGN KEY ("cabinet_id") REFERENCES "cabinets"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "channels" ADD CONSTRAINT "channels_cluster_id_fkey" FOREIGN KEY ("cluster_id") REFERENCES "clusters"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "messages" ADD CONSTRAINT "messages_channel_id_fkey" FOREIGN KEY ("channel_id") REFERENCES "channels"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "message_reactions" ADD CONSTRAINT "message_reactions_message_id_fkey" FOREIGN KEY ("message_id") REFERENCES "messages"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "message_reactions" ADD CONSTRAINT "message_reactions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "message_reactions" ADD CONSTRAINT "message_reactions_cabinet_id_fkey" FOREIGN KEY ("cabinet_id") REFERENCES "cabinets"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "message_reports" ADD CONSTRAINT "message_reports_message_id_fkey" FOREIGN KEY ("message_id") REFERENCES "messages"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
